54652db788de2225dc5eb1a2c2f25652e29b50d0
ZINBI Mourad <zinbimourad149@gmail.com>
2020-09-20 14:43:25 +0100

    first commit

---
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -0,0 +1,13 @@
+package com.example.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+
+}
